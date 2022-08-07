bd0847733893f5bc11e0408daf0ddf2c0d0ebea0
mandara11 <mandaram2345@gmail.com>
2022-08-07 20:26:55 +0530

    Added:UC3

--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@
             //obj.createContactMethod();
             //obj.printContact();
 
-            AddContactUserInput obj2 = new AddContactUserInput();//UC2
-            obj2.addContact();
-            obj2.printAddedContact();   
+            //AddContactUserInput obj2 = new AddContactUserInput();//UC2
+            //obj2.addContact();
+            //obj2.printAddedContact();
+
+            EditContact obj3 = new EditContact(); //UC3
+            obj3.editContacts();
 
         }
     }
