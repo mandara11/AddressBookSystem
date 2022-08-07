f1025d983fed9f7570bb96e76b21d0c15822fcd6
mandara11 <mandaram2345@gmail.com>
2022-08-07 20:02:42 +0530

    Added:UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Program");
+            CreateContact obj = new CreateContact();
+            obj.createContactMethod();
+            obj.printContact();
+
         }
     }
 }
